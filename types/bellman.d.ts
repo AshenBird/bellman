27caab8619faa681ff56a7068e0d4fd267e21bbd
mcswift <develop@mcswift.cn>
2023-09-16 11:33:43 +0800

    update v0.0.3

--- types/bellman.d.ts
+++ types/bellman.d.ts
@@ -1,8 +1,19 @@
+type CommonVoid = void | Promise<void>;
+type ObserverRecord<T> = {
+    next: (val: T) => CommonVoid;
+    error?: (err: any) => CommonVoid;
+};
+type Observer<T> = ObserverRecord<T> | ((val: T) => CommonVoid);
 export declare class Bellman<T = void> {
     private promise;
-    private subject;
-    constructor();
+    private value;
+    private subscribeMap;
     get signal(): Promise<T>;
+    constructor(val: T);
+    subscribe(observer: Observer<T>): () => void;
+    next(val: T): void;
+    error(err: any): void;
     resolve(info: T): void;
     reject(err: any): void;
 }
+export {};
